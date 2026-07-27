f907dae04ca4597ece7cc3101a7c9fb9ab4368a6
Somil sahu <somilsahu086@gmail.com>
2026-07-27 20:49:35 +0530

    1_changes

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,15 +3,13 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
 def load_and_clean_data():
-    # 1. Fetch Data
+
     housing = fetch_california_housing(as_frame=True)
     df = housing.frame
     df = df.rename(columns={'MedHouseVal': 'HouseValue'})
     
-    # 2. Handle Capping Outlier
     df = df[df["HouseValue"] < 5.0]
     
-    # 3. Feature Engineering
     df["RoomsPerPerson"] = df["AveRooms"] / df["AveOccup"]
     df["BedroomsPerRoom"] = df["AveBedrms"] / df["AveRooms"]
     
